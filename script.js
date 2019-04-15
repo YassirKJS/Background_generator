216789c8afeb93d768485fee9b14821e650ace14
Yassir Khchaf <khchafyassirJS@gmail.com>
2019-04-15 13:48:42 +0200

    Added the setGradient

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var h3 = document.querySelector("h3");
+const css = document.querySelector("h3");
+const color1 = document.getElementById("color1");
+const color2 = document.getElementById("color2");
+const body = document.querySelector("body");
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + " ," + color2.value +")";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
